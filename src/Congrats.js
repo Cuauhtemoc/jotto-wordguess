671022194c6ee6e96c439c3708ff17a92bc6c56e
Cuauhtemoc Rodriguez <crodri606@gmail.com>
2019-08-11 18:36:55 -0500

    add proptypes

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default (props) => {
+const Congrats = (props) => {
     if (props.success) {
         return(
         <div data-test='component-congrats'>
@@ -17,9 +17,11 @@ export default (props) => {
 
             </div>
         )
-    }
-    
-
-    
+    }    
 }
 
+Congrats.propTypes = {
+    success: PropTypes.bool.isRequired,
+}
+
+export default Congrats;
--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import Enzyme, {shallow} from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
+import checkPropTypes from 'check-prop-types';
 
-import {findByTestAttr} from '../test/testUttils';
+import {findByTestAttr, checkProps} from '../test/testUttils';
 import Congrats from './Congrats';
 
 Enzyme.configure({adapter: new EnzymeAdapter()  });
 
+const defaultfProps = {success:false};
 const setup = (props={}) => {
-    return shallow(<Congrats {...props} />)
+    const setupProps = {...defaultfProps, ...props};
+    return shallow(<Congrats {...setupProps} />)
 }
 
 test('renders without error', () => {
-    const wrapper = setup();
+    const wrapper = setup({success:false});
     const component = findByTestAttr(wrapper, 'component-congrats')
     expect(component.length).toBe(1);
 
@@ -28,6 +31,11 @@ test('renders non-empty congrats msg when success prop is true', () => {
     const wrapper = setup({success:true});
     const message = findByTestAttr(wrapper, 'congrats-message');
     expect(message.text().length).not.toBe(0);
+});
+
+test('does not throw warning with expected props', () => {
+    const expectedProps = {success: false};
+    checkProps(Congrats, expectedProps);
 });
 
 
